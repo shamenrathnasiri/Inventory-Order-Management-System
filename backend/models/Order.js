115bf275ecb8c0661f131846a53c8bf8ed72e19b
Bhanuka Shamen Rathnasiri <120768729+shamenrathnasiri@users.noreply.github.com>
2025-06-05 08:59:24 +0530

    setup backend

---
+++ backend/models/Order.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  products: [{ 
+    productId: String,
+    name: String,
+    quantity: Number,
+    price: Number
+  }],
+  totalAmount: Number,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
