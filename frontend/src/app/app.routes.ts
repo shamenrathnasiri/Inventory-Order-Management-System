190910a452ed6b60205fef8e54d676a49cd82c94
Bhanuka Shamen Rathnasiri <120768729+shamenrathnasiri@users.noreply.github.com>
2025-07-14 11:26:03 +0530

    add navigate pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './features/products/product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  
+  {path: '', component: HomeComponent, pathMatch:'full'},
   { path: 'products', component: ProductListComponent },
+  {path: 'about', component:AboutComponent}
   // Add other routes as needed
 ];
