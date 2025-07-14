e56d11fae663b6d884ab340ab89ee0d5bba0b3e0
Bhanuka Shamen Rathnasiri <120768729+shamenrathnasiri@users.noreply.github.com>
2025-07-14 10:37:54 +0530

    add nav bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductService } from './core/services/product.service';
-import { ProductFormComponent } from './features/products/product-form/product-form.component';
 import { ProductListComponent } from "./features/products/product-list/product-list.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [RouterOutlet, ProductListComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,8 @@
-
 import { Routes } from '@angular/router';
-import { ProductFormComponent } from './features/products/product-form/product-form.component';
 import { ProductListComponent } from './features/products/product-list/product-list.component';
 
-
 export const routes: Routes = [
-  { path: 'products', component: ProductFormComponent },
-  { path: 'productlist', component: ProductListComponent },
-
+  
+  { path: 'products', component: ProductListComponent },
+  // Add other routes as needed
 ];
-
